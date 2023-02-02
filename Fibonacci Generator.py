781520410decc6fba932f4d1e2a297c4c28e4260
SUDHARSANAN S R <109327528+sudharsanan123@users.noreply.github.com>
2023-02-02 21:57:51 +0530

    Create Fibonacci Generator.py

---
+++ Fibonacci Generator.py
@@ -0,0 +1,23 @@
+
+def  fibonacci(n):
+
+	if n == 1  or n == 0:
+
+		return n;
+
+	else:
+
+		return fibonacci(n-2) + fibonacci(n - 1)
+
+
+numero = int(input("ingrese un numero entero  positivo: "))
+
+if numero < 0:
+	print("Numero no valido")
+
+i = 0
+
+print("Secesion de fibonacci: ")
+
+for i in range(0, numero):
+	print(fibonacci(i))
